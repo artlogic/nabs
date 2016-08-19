b77d62ee1325991d9afa5d46cf43d26d79a3ae00
James Kruth <james@arsmentis.com>
2016-08-19 02:12:45 -0400

    Fixing a few things for node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "parserOptions": {
+    "sourceType": "script"
+  },
+  "rules": {
+    "strict": [
+      2,
+      "safe"
+    ],
+  }
 };
